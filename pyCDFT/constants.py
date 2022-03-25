6bfedd9524f5ffdeb74256bf4c6f1965abb45b84
Morten Hammer <morten.hammer@ntnu.no>
2022-03-25 17:49:12 +0100

    Added plot constants

--- pyCDFT/constants.py
+++ pyCDFT/constants.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import OrderedDict
 
 # How to do convolution integrals
 CONV_FFTW = 1
@@ -17,6 +18,28 @@ KB = 1.380650524e-23
 RGAS = NA*KB
 # Planck’s constant
 h = 6.626069311e-34
+
+
+# Plot utility
+LINE_STYLES = OrderedDict(
+    [('0',               (0, ())),
+     ('1',              (0, (5, 5))),
+     ('2',              (0, (1, 5))),
+     ('3',          (0, (3, 5, 1, 5))),
+     ('4',         (0, (3, 5, 1, 5, 1, 5))),
+     ('5',      (0, (1, 10))),
+     ('6',      (0, (5, 10))),
+     ('7',  (0, (3, 10, 1, 10))),
+     ('8', (0, (3, 10, 1, 10, 1, 10))),
+     ('9',      (0, (1, 1))),
+     ('10',      (0, (5, 1))),
+     ('11',  (0, (3, 1, 1, 1))),
+     ('12', (0, (3, 1, 1, 1, 1, 1)))])
+
+LCOLORS = ["k", "g", "r", "b", "grey", "m", "c", "y",
+           "tomato", "peru", "olive", "royalblue", "darkviolet"]
+LMARKERS = ["*", "d", "o", "s", "+", "x", "1", "h", "2", "v", "p", "^", "X"]
+
 
 if __name__ == "__main__":
 
