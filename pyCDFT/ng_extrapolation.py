1a0130ece38cf44554655a89bb3a9ba51fd59456
Morten Hammer <morten.hammer@ntnu.no>
2022-03-25 17:45:51 +0100

    Adapted Ng for multi-components

--- pyCDFT/ng_extrapolation.py
+++ pyCDFT/ng_extrapolation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
+from utility import densities
+
 
 class ng_nc_wrapper():
     """
     Wrap ng_extrapolation for nc components
     """
 
-    def __init__(self, nc, N, n_update,  array_mask=None):
+    def __init__(self, nc, N, n_update,  array_mask):
         """
 
         Args:
@@ -16,20 +18,37 @@ class ng_nc_wrapper():
             array_mask (np.ndarray of bool): Mask array calculations
         """
         self.nc = nc
-        self.ng = []
-        for _ in range(nc):
-            self.ng.append(ng_extrapolation(N=N, n_update=n_update, array_mask=array_mask))
+        self.array_mask = array_mask
+        self.densities = densities(nc, N)
+        self.i0 = None
+        self.iN = None
+        for i in range(N):
+            if array_mask[i]:
+                self.i0 = i
+                break
+        for i in range(N):
+            if array_mask[N-1-i]:
+                self.iN = N-1-i
+                break
+        self.N_masked = self.iN - self.i0 + 1
+        self.fn = np.zeros(self.N_masked*nc)
+        self.gn = np.zeros(self.N_masked*nc)
+        self.ng = ng_extrapolation(N=self.N_masked*nc, n_update=n_update)
 
     def push_back(self, fn, gn, iteration):
         """
 
         Args:
-            fn (np.ndarray): Variable of iterative method
-            gn (np.ndarray): gn = A fn
+            fn (densities): Variable of iterative method
+            gn (densities): gn = A fn
             iteration (int): Current iteration
         """
         for i in range(self.nc):
-            self.ng[i].push_back(fn[i], gn[i], iteration)
+            self.fn[self.N_masked*i:self.N_masked *
+                    (i+1)] = fn[i][self.array_mask]
+            self.gn[self.N_masked*i:self.N_masked *
+                    (i+1)] = gn[i][self.array_mask]
+        self.ng.push_back(self.fn, self.gn, iteration)
 
     def time_to_update(self, iteration):
         """
@@ -40,15 +59,17 @@ class ng_nc_wrapper():
         Returns:
             (bool): True if it is time to update
         """
-        return self.ng[0].time_to_update(iteration)
+        return self.ng.time_to_update(iteration)
 
     def extrapolate(self):
         """
         """
-        extrapolation = []
+        extrapolation = self.ng.extrapolate()
         for i in range(self.nc):
-            extrapolation.append(self.ng[i].extrapolate())
-        return extrapolation
+            self.densities[i][self.array_mask] = extrapolation[self.N_masked *
+                                                               i:self.N_masked * (i+1)]
+        return self.densities
+
 
 class ng_extrapolation():
     """
@@ -81,7 +102,7 @@ class ng_extrapolation():
         self.dnm2 = np.zeros(N)
         # Mask calculations
         if array_mask is None:
-            self.array_mask = np.full(N, False, dtype=bool)
+            self.array_mask = np.full(N, True, dtype=bool)
         else:
             self.array_mask = array_mask
 
@@ -93,16 +114,18 @@ class ng_extrapolation():
             gn (np.ndarray): gn = A fn
             iteration (int): Current iteration
         """
-        if self.n_update is None: return
+        if self.n_update is None:
+            return
         # No need to copy arrays that will not be used:
-        if iteration%self.n_update == 0 or iteration%self.n_update > self.n_update - 3:
+        if iteration % self.n_update == 0 or iteration % self.n_update > self.n_update - 3:
             self.gnm2[self.array_mask] = self.gnm1[self.array_mask]
             self.gnm1[self.array_mask] = self.gn[self.array_mask]
             self.gn[self.array_mask] = gn[self.array_mask]
 
             self.dnm2[self.array_mask] = self.dnm1[self.array_mask]
             self.dnm1[self.array_mask] = self.dn[self.array_mask]
-            self.dn[self.array_mask] = self.gn[self.array_mask] - fn[self.array_mask]
+            self.dn[self.array_mask] = self.gn[self.array_mask] - \
+                fn[self.array_mask]
 
     def extrapolate(self):
         """
@@ -123,7 +146,8 @@ class ng_extrapolation():
         b /= scaling
         c = np.linalg.solve(a, b)
         fnp1 = (1 - c[0] - c[1]) * self.gn[self.array_mask] + \
-               c[0] * self.gnm1[self.array_mask] + c[1] * self.gnm2[self.array_mask]
+            c[0] * self.gnm1[self.array_mask] + \
+            c[1] * self.gnm2[self.array_mask]
         return fnp1
 
     def time_to_update(self, iteration):
@@ -141,8 +165,9 @@ class ng_extrapolation():
         elif iteration < 3:
             update = False
         else:
-            update = iteration%self.n_update == 0
+            update = iteration % self.n_update == 0
         return update
+
 
 if __name__ == "__main__":
     pass
